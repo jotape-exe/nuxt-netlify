8ce5bb00f35beabeae272f104d3e59e38677725f
Gleyson Sampaio <80013043+glysns@users.noreply.github.com>
2023-10-31 16:38:35 -0300

    public api via cep

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,8 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   runtimeConfig:{
-    viaCepApi:process.env.VIACEP_API
+    public:{
+      viaCepApi:process.env.VIACEP_API
+    }
   }
 })
