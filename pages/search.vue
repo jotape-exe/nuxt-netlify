4ac24cfeaf4b2c2b60cbfe7cca9263621dfaa290
Gleyson Sampaio <80013043+glysns@users.noreply.github.com>
2023-11-01 11:30:18 -0300

    ajustes

--- pages/search.vue
+++ pages/search.vue
@@ -1,11 +1,36 @@
 <template>
     <div>
         <h1>Search Event</h1>
+        Nome: <input type="text" v-model="filtro" /> {{ filtro }}
+        <button @click="buscar">Buscar</button>
 
+        <br><br>
+        Estados Localizados
+        <br><br>
+        <ul>
+            <li v-for="i in ufs">
+                {{ i }}
+            </li>
+        </ul>
     </div>
 </template>
 
 <script setup>
+const filtro = ref()
+const ufs = ref()
+const buscar = async ()  => {
+    //const { data:stats } = await useAsyncData( 'stats', () => $fetch( config.API_BASE_URL+'/stats') );
+
+    const { data, pending, error, refresh } = await useAsyncData(
+  'ufs',
+  () => $fetch('/api/constants/ufs')
+)
+
+    console.log(data)
+    ufs.value = data.value;
+    console.log(ufs)
+    
+}
 
 </script>
 
