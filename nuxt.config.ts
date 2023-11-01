3e0867482cb7df74400086918b5392306950cc10
Gleyson Sampaio <80013043+glysns@users.noreply.github.com>
2023-11-01 15:08:30 -0300

    MODULARIZANDO COMPOSABLES

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,5 +7,26 @@ export default defineNuxtConfig({
       VIACEP_API:'',
       SYSTEM_API:'',
     }
+  },
+  imports: {
+    dirs: [
+      // Scan top-level modules
+      // 'composables',
+      // ... or scan modules nested one level deep with a specific name and file extension
+      // 'composables/*/index.{ts,js,mjs,mts}',
+      // ... or scan all modules within given directory
+      'composables/**'
+    ]
   }
 })
+
+
+/*
+
+components: {
+    global: true,
+    dirs: ['~/components']
+  },
+
+  https://vueschool.io/articles/vuejs-tutorials/understanding-the-directory-structure-in-nuxt-3/
+  */
