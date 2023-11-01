b4cfbbf4b62fc53be49dafb03fa04a721f8815d6
Gleyson Sampaio <80013043+glysns@users.noreply.github.com>
2023-11-01 01:02:54 -0300

    ajustes

--- server/api/users/[...slug].js
+++ server/api/users/[...slug].js
@@ -5,5 +5,5 @@ const router = createRouter()
 
 router.get('/all', defineEventHandler ( () => db.ufs ))
 
-console.log(db.ufs)
+//console.log(db.ufs)
 export default useBase('/api/users', router.handler)
--- server/routes/catalogos/index.js
+++ server/routes/catalogos/index.js
@@ -1,6 +1,6 @@
 export default defineEventHandler( async (event) => {
     const config = useRuntimeConfig();
-    console.log('App Config', config)
+    //console.log('App Config', config)
     return {key:'TOKEN', value:config.TOKEN}
 
 })
